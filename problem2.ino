293cf67f142b2b2bb81ac123c118c7e51633c614
gsyring <113934340+gsyring@users.noreply.github.com>
2022-12-14 11:27:33 -0700

    Create problem2.ino
    NOTE: USES PINS 11,9,and 6 INSTEAD OF 11,10,and 9

---
+++ problem2.ino
@@ -0,0 +1,15 @@
+/*
+
+*/
+
+void setup() {
+  pinMode(11,OUTPUT);
+  pinMode(9,OUTPUT);
+  pinMode(6,OUTPUT);
+}
+
+void loop() {
+  analogWrite(9,2);
+  analogWrite(,16);
+  analogWrite(11,128);
+}
